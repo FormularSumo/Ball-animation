aad5b477fd12eb907b8279e84d9aa6ba2074cdc8
James <james.heppell22@pgs.org.uk>
2020-09-12 12:45:30 +0000

    Stable

--- main.py
+++ main.py
@@ -2,26 +2,25 @@ from tkinter import *
 import time
 
 
-def up(self):
-  self.dy = -5
-  self.dx = 0
-  print(self.name + " up_button pressed")
-  self.update()
+def up():
+  Ball_1.dy = -5
+  Ball_1.dx = 0
+  Ball_1.update()
 
 def right():
-  ball1.dy = 0
-  ball1.dx = 5
-  ball1.update()
+  Ball_1.dy = 0
+  Ball_1.dx = 5
+  Ball_1.update()
 
 def left():
-  ball1.dy = 0
-  ball1.dx = -5
-  ball1.update()
+  Ball_1.dy = 0
+  Ball_1.dx = -5
+  Ball_1.update()
 
 def down():
-  ball1.dy = 5
-  ball1.dx = 0
-  ball1.update()
+  Ball_1.dy = 5
+  Ball_1.dx = 0
+  Ball_1.update()
 
 root = Tk()
 frame = Frame(root)
@@ -30,17 +29,17 @@ frame.grid()
 bottomframe = Frame(root)
 bottomframe.grid(row=1, column=1, columnspan=1)
 
-# up_button = Button(frame, text="UP", fg="red", command=up)
-# up_button.grid(row=1, column=2, columnspan=1)
+up_button = Button(frame, text="UP", fg="red", command=up)
+up_button.grid(row=1, column=2, columnspan=1)
 
-# right_button = Button(frame, text="RIGHT", fg="red", command=right)
-# right_button.grid(row=2, column=3, columnspan=1)
+right_button = Button(frame, text="RIGHT", fg="red", command=right)
+right_button.grid(row=2, column=3, columnspan=1)
 
-# left_button = Button(frame, text="LEFT", fg="red", command=left)
-# left_button.grid(row=2, column=1, columnspan=1)
+left_button = Button(frame, text="LEFT", fg="red", command=left)
+left_button.grid(row=2, column=1, columnspan=1)
 
-# down_button = Button(frame, text="DOWN", fg="red", command=down)
-# down_button.grid(row=2, column=2, columnspan=1)
+down_button = Button(frame, text="DOWN", fg="red", command=down)
+down_button.grid(row=2, column=2, columnspan=1)
 
 
 
@@ -64,9 +63,6 @@ class ball:
     self.ball = canvas.create_oval(x0,y0,x1,y1,outline = self.outline, fill = self.fill)
     canvas.pack()
 
-    self.up_button = Button(frame, text=self.name + " UP", fg=self.fill, command=up(self))
-    self.up_button.grid(row=1, column=2+2*self.number, columnspan=1)
-
   
   def update(self):
     time.sleep(0.02)
@@ -81,24 +77,24 @@ Ball_2 = ball('Ball 2', 1, 380, 280, 420, 320,'white','red')
 
 
 # while True:
-#   ball1.update()
+#   Ball_1.update()
 #   ball2.update()
 
 
 
 #track = 0
 
-# def animation(track,ball1_direction):
+# def animation(track,Ball_1_direction):
 #   while True:
-#       ball1x = 6
+#       Ball_1x = 6
 #       ball2x = 3
-#       ball1y = 3
+#       Ball_1y = 3
 #       ball2y = 1.5
-#       print(ball1_direction)
+#       print(Ball_1_direction)
 #       if track == 0:
 #           for i in range(0,80):
 #               time.sleep(0.02)
-#               canvas.move(ball1, ball1x, ball1y)
+#               canvas.move(Ball_1, Ball_1x, Ball_1y)
 #               canvas.move(ball2, ball2x, ball2y)
 #               canvas.update()
 #           track = 1
@@ -106,7 +102,7 @@ Ball_2 = ball('Ball 2', 1, 380, 280, 420, 320,'white','red')
 #       elif track == 1:
 #           for i in range(0,80):
 #               time.sleep(0.02)
-#               canvas.move(ball1, -ball1x, -ball1y)
+#               canvas.move(Ball_1, -Ball_1x, -Ball_1y)
 #               canvas.move(ball2, -ball2x, -ball2y)
 #               canvas.update()
 #           track = 0
